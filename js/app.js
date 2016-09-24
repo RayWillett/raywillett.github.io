6151639d42a59cdf32f6f7191e47d998252fdae3
Raymond <rwillet2@emich.edu>
2016-09-23 20:14:20 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,58 @@
+particlesJS('particles',
+  
+  {
+    "particles": {
+      "number": {
+        "value": 100,
+        "density": {
+          "enable": true,
+          "value_area": 600
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#ffffff"
+        },
+        "polygon": {
+          "nb_sides": 5
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 200,
+        "color": "#ffffff",
+        "opacity": 0.1,
+        "width": 2
+      },
+      "move": {
+        "enable": true,
+        "speed": 0
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "modes": {
+        "grab": {
+          "distance": 200,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+      }
+    },
+    "retina_detect": false
+  }
+);
